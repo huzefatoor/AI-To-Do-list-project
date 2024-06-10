e41baebe7d7f062f44f9674a688680625407028a
Huzefa Toor <115381625+huzefatoor@users.noreply.github.com>
2024-06-10 10:17:01 -0400

    Add files via upload

---
+++ wave_examples.py
@@ -0,0 +1,21 @@
+# Audio file formats
+# .mp3 
+# .flac 
+# .wav
+
+import wave
+
+# Audio signal parameters 
+# - number of channels 
+# - sample width 
+# - framerate/sample_rate 
+# - number of frames 
+# - values of frame
+
+# loads audio file with wave.open()
+obj = wave.open("output.mp3", "rb")
+
+# print parameters
+print(obj.getframerate())
+print(obj.getnchannels())
+print(obj.getnframes())
